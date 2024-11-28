0ecacd9dbcd758ce33021da2099f2f364d0f1b8f
0xtanmaye <106451224+0xtanmaye@users.noreply.github.com>
2024-11-28 14:21:43 +0530

    Update practical_4_bubble_sort.c to add new logic

--- practical_4_bubble_sort.c
+++ practical_4_bubble_sort.c
@@ -20,24 +20,40 @@ int main()
 		printf("%d\t", n[i]);
 	}
 	
-	for(i=size-1;i>0;i--)
-	{
-		for(j=0;j<i;j++)
-		{
-			if(n[j]>n[j+1])
-			{
-				temp=n[j];
-				n[j]=n[j+1];
-				n[j+1]=temp;
-			}
-		}
-	}
+	/* Alternative logic
+        for(i=size-1;i>0;i--)
+        {
+                for(j=0;j<i;j++)
+                {
+                        if(n[j]>n[j+1])
+                        {
+                                temp=n[j];
+                                n[j]=n[j+1];
+                                n[j+1]=temp;
+                        }
+                }
+        }
+        */
+
+        for(i=0;i<(size-1);i++)
+        {
+                for(j=0;j<(size-i-1);j++)
+                {
+                        if(n[j]>n[j+1])
+                        {
+                                temp=n[j];
+                                n[j]=n[j+1];
+                                n[j+1]=temp;
+                        }
+                }
+        }
 	
 	printf("\n\nEntered elements after bubble sort:\n");
 	for(i=0;i<size;i++)
 	{
 		printf("%d\t", n[i]);
 	}
+	printf("\n");
 	
 	return 0;
 }
